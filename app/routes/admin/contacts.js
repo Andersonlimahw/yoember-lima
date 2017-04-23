6a820bf72b7badf966cbd5a1b19adaaa75fe5602
andersonlimahw <andersonlimahw@gmail.com>
2017-04-23 05:18:58 -0300

    homeowrk 4 , delete contact and invitation

--- app/routes/admin/contacts.js
+++ app/routes/admin/contacts.js
@@ -2,6 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(){
-        return this.store.findAll('contact'); // return all contacts        
+        return this.store.findAll('contact'); // return all contacts
+    },
+    actions: {
+
+    deleteContact(contact) {
+      let confirmation = confirm('Are you sure?');
+
+      if (confirmation) {
+        contact.destroyRecord();
+      }
     }
+  }
 });
--- app/routes/admin/invitations.js
+++ app/routes/admin/invitations.js
@@ -3,5 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model(){
         return this.store.findAll('invitation'); // invitaion is our model for invitations
+    },
+    actions: {
+
+    deleteInvitation(invitation) {
+      let confirmation = confirm('Are you sure?');
+
+      if (confirmation) {
+        invitation.destroyRecord();
+      }
     }
+  }
 });
