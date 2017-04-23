f6530dd4f9520810239db1b0e3b1b5ac6d4006a0
andersonlimahw <anderson.lima@soaexpert.com.br>
2017-04-23 01:30:07 -0300

    sync proj

--- dist/assets/yo-app.js
+++ dist/assets/yo-app.js
@@ -468,7 +468,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("yo-app/app")["default"].create({"LOG_RESOLVER":true,"LOG_ACTIVE_GENERATION":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true,"LOG_VIEW_LOOKUPS":true,"name":"yo-app","version":"0.0.0+b38b466a"});
+  require("yo-app/app")["default"].create({"LOG_RESOLVER":true,"LOG_ACTIVE_GENERATION":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true,"LOG_VIEW_LOOKUPS":true,"name":"yo-app","version":"0.0.0+dfd34bc7"});
 }
 
 /* jshint ignore:end */
