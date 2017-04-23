04523b1f1ba1d11292e38ae4604924ea5f2931c9
andersonlimahw <andersonlimahw@gmail.com>
2017-04-23 02:50:55 -0300

    deploy , firebase

--- dist/assets/yo-app.js
+++ dist/assets/yo-app.js
@@ -468,7 +468,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("yo-app/app")["default"].create({"LOG_RESOLVER":true,"LOG_ACTIVE_GENERATION":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true,"LOG_VIEW_LOOKUPS":true,"name":"yo-app","version":"0.0.0+f6530dd4"});
+  require("yo-app/app")["default"].create({"LOG_RESOLVER":true,"LOG_ACTIVE_GENERATION":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true,"LOG_VIEW_LOOKUPS":true,"name":"yo-app","version":"0.0.0+71d1f816"});
 }
 
 /* jshint ignore:end */
